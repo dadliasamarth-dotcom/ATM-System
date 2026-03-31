768fd7b468871d0003b305dc0dbd8d513eeac9fd
Samarth Dadlia <dadliasamarth@gmail.com>
2026-03-31 20:03:23 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,6 +1,6 @@
 User = int(input("Enter a Pin "))
 pin = 1234
-Balance = 2,00,000
+Balance = 100000.00
 
 if User == pin:
     print("\n1. Check balance \n2. Deposit \n3. Withdraw \n4. Exit")
@@ -9,20 +9,21 @@ if User == pin:
 
     match choice:
             case 1:
-                print("Your Balance is:", Balance)
+                print(f"Your Balance is: ₹{Balance:,.2f}")
             case 2:
-                a = int(input("Enter amount to deposit: "))
+                a = float(input("Enter amount to deposit: "))
                 Balance = Balance + a
-                print("Deposit successful! Updated balance is:", Balance)
+                print(f"Deposit successful! Updated balance is: ₹{Balance:,.2f}")
             case 3:
-                b = int(input("Enter amount to withdraw: "))
+                b = float(input("Enter amount to withdraw: "))
                 if Balance < b:
                     print("Insufficient balance")
                 else:
                     Balance = Balance - b
-                    print("Withdrawal successful! Upadted balance is:", Balance)
+                    print(f"Withdrawal successful! Updated balance is: ₹{Balance:,.2f}")
             case 4:
                 print("Thank you for using ATM. Goodbye!")
     
 else:
     print("Incorrect PIN. Please try again.")
+
